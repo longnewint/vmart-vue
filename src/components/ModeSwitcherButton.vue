6de9e167a43060e8fb7c13e3923a13c25f097f07
Long Nguyen <longnewint@gmail.com>
2025-09-07 22:23:38 -0400

    Add ModeSwitcherButton

---
+++ src/components/ModeSwitcherButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <Button :icon="iconProps" severity="secondary" @click="toggleDarkMode" aria-label="switcherButton"></Button>
+</template>
+<script setup>
+import { ref, computed } from "vue";
+
+const isDarkMode = ref(false);
+toggleDarkMode();
+
+function toggleDarkMode() {
+  isDarkMode.value = !isDarkMode.value;
+  document.documentElement.classList.toggle('my-app-dark');
+}
+
+const iconProps = computed(() => isDarkMode.value ? 'pi pi-sun' : 'pi pi-moon');
+</script>
