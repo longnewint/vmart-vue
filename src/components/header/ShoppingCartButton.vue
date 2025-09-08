caa9cd86854cb3febb3bbfa1084ad5e71023c4c4
Long Nguyen <longnewint@gmail.com>
2025-09-08 08:31:41 -0400

    Add ShoppingCartButton

---
+++ src/components/header/ShoppingCartButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <Button type="button" icon="pi pi-shopping-cart" :badge="itemNumString" badgeSeverity="contrast" variant="outlined" />
+</template>
+<script setup>
+import { computed, ref } from "vue";
+
+const itemNum = ref("0");
+const itemNumString = computed(() => itemNum.value.toString());
+</script>
