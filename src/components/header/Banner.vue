df0b5ad7472264a365daecb9cb250790f18fe401
Long Nguyen <longnewint@gmail.com>
2025-09-09 23:10:35 -0400

    Add Banner

---
+++ src/components/header/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="pb-20 bg-surface-0 dark:bg-surface-950">
+        <div class="bg-surface-900 text-surface-100 py-4 px-6 lg:px-20 flex justify-between items-center flex-wrap">
+            <div class="font-bold">🔥 Hot Deals!</div>
+            <div class="inline-flex gap-1 items-center">
+                <span class="hidden lg:flex leading-normal">Libero voluptatum atque exercitationem praesentium provident odit.</span>
+                <a class="text-surface-0 underline font-bold" href="#">Learn More</a>
+            </div>
+            <Button icon="pi pi-times" text rounded severity="secondary" class="text-surface-0! hover:bg-surface-500/20!" />
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
