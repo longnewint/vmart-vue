acd0be2343818e137f5ce839004480d037f6010f
Long Nguyen <longnewint@gmail.com>
2025-09-15 23:27:03 -0400

    Change Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex flex-row justify-evenly flex-wrap">
+    <div class="flex flex-col">
+      <Button label="About vMart delivery" variant="text" severity="contrast"/>
+      <Button label="FAQs" variant="text" severity="contrast"/>
+      <Button label="Delivery & Pickup Fees" variant="text" severity="contrast"/>
+      <Button label="Return Policy" variant="text" severity="contrast"/>
+      <Button label="Getting Help" variant="text" severity="contrast"/>
+    </div>
+    <div class="flex flex-col">
+      <Button label="Weekly Flyer" variant="text" severity="contrast"/>
+      <Button label="Store Locator" variant="text" severity="contrast"/>
+      <Button label="Gift Cards" variant="text" severity="contrast"/>
+    </div>
+    <div class="flex flex-col">
+      <Button label="More Services" variant="text" severity="contrast"/>
+      <Button label="Loyalty Program" variant="text" severity="contrast"/>
+      <Button label="vMart Finance" variant="text" severity="contrast"/>
+      <Button label="Pharmacy" variant="text" severity="contrast"/>
+      <Button label="Advertising with us" variant="text" severity="contrast"/>
+      <Button label="Sell on vMart" variant="text" severity="contrast"/>
+    </div>
+    <div class="flex flex-col">
+      <Button label="About Us" variant="text" severity="contrast"/>
+      <Button label="Who We Are" variant="text" severity="contrast"/>
+      <Button label="Legal" variant="text" severity="contrast"/>
+      <Button label="Sustainability" variant="text" severity="contrast"/>
+      <Button label="Charity & Community" variant="text" severity="contrast"/>
+      <Button label="Careers" variant="text" severity="contrast"/>
+      <Button label="Contact Us" variant="text" severity="contrast"/>
+    </div>
+  </div>
+
+
+  <div class="flex flex-row justify-end">
+    <div><Button label="Privacy Policy" variant="text" severity="secondary"/></div>
+    <div><Button label="Terms of Use" variant="text" severity="secondary"/></div>
+    <div><Button label="@vMart Inc. All Rights Reserved." variant="text" severity="secondary"/></div>
+  </div>
+</template>
+
+<script setup></script>
