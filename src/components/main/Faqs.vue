3b0f116fabbd5815f289476ddbcf4e207db1eb69
Long Nguyen <longnewint@gmail.com>
2025-09-15 23:25:39 -0400

    Change Faqs

--- src/components/main/Faqs.vue
+++ src/components/main/Faqs.vue
@@ -1,20 +1,24 @@
 <template>
   <Card style="max-width: 64rem">
     <template #title>Frequently Asked Questions</template>
-    <template #subtitle>Excepteur sint occaecat cupidatat non proident, suntin culpa qui officia deserunt mollit anim id
+    <template #subtitle>Excepteur sint occaecat cupidatat non proident, suntin culpa qui officia deserunt mollit anim
+      id
       est laborum.</template>
     <template #content>
-      <div class="card">
+      <div>
         <Accordion :value="['0']" multiple>
           <AccordionPanel value="0">
             <AccordionHeader>Header I</AccordionHeader>
             <AccordionContent>
               <p class="m-0">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip
                 ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla
+                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+                anim
                 id est laborum.
               </p>
             </AccordionContent>
@@ -23,11 +27,13 @@
             <AccordionHeader>Header II</AccordionHeader>
             <AccordionContent>
               <p class="m-0">
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium,
+                totam
                 rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
                 explicabo. Nemo enim
                 ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-                eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
+                eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius
+                modi.
               </p>
             </AccordionContent>
           </AccordionPanel>
@@ -39,7 +45,8 @@
                 deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non
                 provident, similique sunt in culpa
                 qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est
-                et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo
+                et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit
+                quo
                 minus.
               </p>
             </AccordionContent>
