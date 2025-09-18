a6da7dfeb8d0deb14f5b0372184273ed860c8829
Long Nguyen <longnewint@gmail.com>
2025-09-17 23:48:38 -0400

    Add Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <Hero></Hero>
+  <div class="flex flex-col">
+    <div>
+      <Deals></Deals>
+    </div>
+    <div class="flex justify-center">
+      <Faqs></Faqs>
+    </div>
+  </div>
+</template>
+<script setup></script>
