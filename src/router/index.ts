d0f0fb17c21175eb6d19f405fa2a433ad58272eb
Long Nguyen <longnewint@gmail.com>
2025-09-18 08:59:36 -0400

    Add Category route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Layout from './../layout/Layout.vue'
 import Home from "../views/Home.vue"
+import Category from "../views/Category.vue"
 
 const routes = [
   {
@@ -10,6 +11,10 @@ const routes = [
       {
         path: '/',
         component: Home
+      },
+      {
+        path: 'category/:id',
+        component: Category
       }
     ]
   }
