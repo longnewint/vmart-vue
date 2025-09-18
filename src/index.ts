0c672e4666b16770d534953b9232e3a788a76e2e
Long Nguyen <longnewint@gmail.com>
2025-09-17 23:47:37 -0400

    Change to vue-router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,59 @@
 import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 import App from './App.vue';
-import './index.css';
+import { Button } from 'primevue';
+import { Card } from 'primevue';
+import { OverlayBadge } from 'primevue';
+import { MegaMenu } from 'primevue';
+import { Accordion, AccordionPanel, AccordionHeader, AccordionContent } from 'primevue';
+import { Carousel } from 'primevue';
 
-createApp(App).mount('#root');
+import ModeSwitcherButton from './components/header/ModeSwitcherButton.vue';
+import ShoppingCartButton from './components/header/ShoppingCartButton.vue';
+import Banner from './components/header/Banner.vue';
+
+import Hero from './components/main/Hero.vue';
+import Faqs from './components/main/Faqs.vue';
+import Deals from './components/main/Deals.vue';
+
+import Header from './layout/Header.vue';
+import Footer from './layout/Footer.vue';
+
+import './assets/index.css';
+import 'primeicons/primeicons.css';
+import router from './router';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.my-app-dark',
+      cssLayer: false
+    }
+  }
+});
+
+app.component('Button', Button);
+app.component('Card', Card);
+app.component('OverlayBadge', OverlayBadge);
+app.component('MegaMenu', MegaMenu);
+app.component('Accordion', Accordion);
+app.component('AccordionPanel', AccordionPanel);
+app.component('AccordionHeader', AccordionHeader);
+app.component('AccordionContent', AccordionContent);
+app.component('Carousel', Carousel);
+
+app.component('ModeSwitcherButton', ModeSwitcherButton);
+app.component('ShoppingCartButton', ShoppingCartButton);
+app.component('Banner', Banner);
+app.component('Header', Header);
+app.component('Hero', Hero);
+app.component('Faqs', Faqs);
+app.component('Deals', Deals);
+
+app.component('Footer', Footer);
+
+app.use(router);
+app.mount("#root");
