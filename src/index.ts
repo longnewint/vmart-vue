fec19473ce174204eb8a683451de59ed1108cb93
Long Nguyen <longnewint@gmail.com>
2025-09-21 09:41:28 -0400

    Add pinia config

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,32 @@
-import { createApp } from 'vue';
-import PrimeVue from 'primevue/config';
-import Aura from '@primeuix/themes/aura';
-import App from './App.vue';
-import { Button } from 'primevue';
-import { Card } from 'primevue';
-import { OverlayBadge } from 'primevue';
-import { MegaMenu } from 'primevue';
-import { Accordion, AccordionPanel, AccordionHeader, AccordionContent } from 'primevue';
-import { Carousel } from 'primevue';
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import App from './App.vue'
+import { Button } from 'primevue'
+import { Card } from 'primevue'
+import { OverlayBadge } from 'primevue'
+import { MegaMenu } from 'primevue'
+import { Accordion, AccordionPanel, AccordionHeader, AccordionContent } from 'primevue'
+import { Carousel } from 'primevue'
 
-import ModeSwitcherButton from './components/header/ModeSwitcherButton.vue';
-import ShoppingCartButton from './components/header/ShoppingCartButton.vue';
-import Banner from './components/header/Banner.vue';
+import ModeSwitcherButton from './components/header/ModeSwitcherButton.vue'
+import ShoppingCartButton from './components/header/ShoppingCartButton.vue'
+import Banner from './components/header/Banner.vue'
 
-import Hero from './components/main/Hero.vue';
-import Faqs from './components/main/Faqs.vue';
-import Deals from './components/main/Deals.vue';
+import Hero from './components/main/Hero.vue'
+import Faqs from './components/main/Faqs.vue'
+import Deals from './components/main/Deals.vue'
 
-import Header from './layout/Header.vue';
-import Footer from './layout/Footer.vue';
+import Header from './layout/Header.vue'
+import Footer from './layout/Footer.vue'
 
-import './assets/index.css';
-import 'primeicons/primeicons.css';
-import router from './router';
+import './assets/index.css'
+import 'primeicons/primeicons.css'
 
-const app = createApp(App);
+import router from './router'
+import { createPinia } from 'pinia'
+
+const app = createApp(App)
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
@@ -33,27 +35,34 @@ app.use(PrimeVue, {
       cssLayer: false
     }
   }
-});
+})
 
-app.component('Button', Button);
-app.component('Card', Card);
-app.component('OverlayBadge', OverlayBadge);
-app.component('MegaMenu', MegaMenu);
-app.component('Accordion', Accordion);
-app.component('AccordionPanel', AccordionPanel);
-app.component('AccordionHeader', AccordionHeader);
-app.component('AccordionContent', AccordionContent);
-app.component('Carousel', Carousel);
+app.component('Button', Button)
+app.component('Card', Card)
+app.component('OverlayBadge', OverlayBadge)
+app.component('MegaMenu', MegaMenu)
+app.component('Accordion', Accordion)
+app.component('AccordionPanel', AccordionPanel)
+app.component('AccordionHeader', AccordionHeader)
+app.component('AccordionContent', AccordionContent)
+app.component('Carousel', Carousel)
 
-app.component('ModeSwitcherButton', ModeSwitcherButton);
-app.component('ShoppingCartButton', ShoppingCartButton);
-app.component('Banner', Banner);
-app.component('Header', Header);
-app.component('Hero', Hero);
-app.component('Faqs', Faqs);
-app.component('Deals', Deals);
+app.component('ModeSwitcherButton', ModeSwitcherButton)
+app.component('ShoppingCartButton', ShoppingCartButton)
+app.component('Banner', Banner)
+app.component('Header', Header)
+app.component('Hero', Hero)
+app.component('Faqs', Faqs)
+app.component('Deals', Deals)
 
-app.component('Footer', Footer);
+app.component('Footer', Footer)
 
-app.use(router);
-app.mount("#root");
+// Router config
+app.use(router)
+
+// State management config
+const pinia = createPinia()
+app.use(pinia)
+
+
+app.mount("#root")
