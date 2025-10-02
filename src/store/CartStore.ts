f66d44363250dbfa732b25db700a21dbce090ee6
Long Nguyen <longnewint@gmail.com>
2025-10-01 21:29:41 -0400

    Add Cart Store

---
+++ src/store/CartStore.ts
@@ -0,0 +1,59 @@
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+import { CartApi } from '../api/CartApi'
+
+export type CartItem = {
+  productId: number
+  brand: string
+  productName: string
+  listPrice: number
+  discountPrice: number
+  packageSize: string
+  thumbnailUrl: string
+  quantity: number
+}
+
+export const useCartStore = defineStore('cart', () => {
+  const loading = ref(false)
+  const error = ref()
+  let cartItems = ref<CartItem[] | null>()
+
+  const orderCount = computed(() => cartItems.value?.length)
+  const orderTotal = computed(() => {
+    if (!cartItems.value) return 0
+    else {
+      let total = 0
+      cartItems.value.forEach(item => total += item.quantity * item.listPrice)
+
+      return total.toFixed(2)
+    }
+  })
+
+  async function fetch() {
+    error.value = cartItems.value = null
+    loading.value = true
+
+    try {
+      cartItems.value = await CartApi.getData() as CartItem[]
+    } catch (err: unknown) {
+      error.value = err
+    } finally {
+      loading.value = false
+    }
+  }
+
+  function add(item: CartItem) {
+    let inCart = false
+    cartItems.value?.forEach(i => {
+      if (item.productId === i.productId)
+        i.quantity++
+    })
+
+    if (!inCart) {
+      item.quantity = 1
+      cartItems.value?.push(item)
+    }
+  }
+
+  return { loading, error, orderCount, orderTotal, cartItems, fetch, add }
+})
