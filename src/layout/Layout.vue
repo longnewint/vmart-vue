fbefff4989114f6c4585d041423d41b32899786b
Long Nguyen <longnewint@gmail.com>
2025-10-03 10:52:48 -0400

    Change layout

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,16 +1,8 @@
 <template>
   <Header class="sticky top-0"></Header>
   <RouterView></RouterView>
-  <li v-for="i in cart.cartItems">{{ i.productName}}</li>
-  <p>{{ cart.orderCount}}</p>
-  <p>{{ cart.orderTotal }}</p>
   <Footer></Footer>
 </template>
 
 <script setup>
-import { useCartStore } from '../store/CartStore';
-
-
-const cart = useCartStore()
-cart.fetch()
 </script>
