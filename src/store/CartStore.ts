20823ab8c4ab7fbbd295ca6b10ca61b6a33dbe6c
Long Nguyen <longnewint@gmail.com>
2025-10-03 10:53:02 -0400

    Change cart store

--- src/store/CartStore.ts
+++ src/store/CartStore.ts
@@ -18,18 +18,18 @@ export const useCartStore = defineStore('cart', () => {
   const error = ref()
   let cartItems = ref<CartItem[] | null>()
 
-  const orderCount = computed(() => cartItems.value?.length)
+  const orderCount = computed(() => cartItems.value?.length.toString())
   const orderTotal = computed(() => {
     if (!cartItems.value) return 0
     else {
       let total = 0
       cartItems.value.forEach(item => total += item.quantity * item.listPrice)
 
-      return total.toFixed(2)
+      return `$${total.toFixed(2)}`
     }
   })
 
-  async function fetch() {
+  async function fetchData() {
     error.value = cartItems.value = null
     loading.value = true
 
@@ -45,8 +45,10 @@ export const useCartStore = defineStore('cart', () => {
   function add(item: CartItem) {
     let inCart = false
     cartItems.value?.forEach(i => {
-      if (item.productId === i.productId)
+      if (item.productId === i.productId) {
+        inCart = true
         i.quantity++
+      }
     })
 
     if (!inCart) {
@@ -55,5 +57,12 @@ export const useCartStore = defineStore('cart', () => {
     }
   }
 
-  return { loading, error, orderCount, orderTotal, cartItems, fetch, add }
+  function remove(id: number) {
+    const index = cartItems.value?.findIndex(item => item.productId === id)
+
+    if (index !== -1)
+      cartItems.value?.splice(index as number, 1)
+  }
+
+  return { loading, error, orderCount, orderTotal, cartItems, fetchData, add, remove }
 })
