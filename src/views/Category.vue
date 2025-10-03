96af5002cbfe152a490dff4a859b5db5f753836f
Long Nguyen <longnewint@gmail.com>
2025-10-03 10:53:17 -0400

    Change category view

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,9 +1,39 @@
 <template>
-  <h1>Category Component</h1>
-  <li v-for="p in products">
-    {{ p.productName }}
-    <Button icon="pi pi-plus" @click="cart.add(p)"></Button>
-  </li>
+  <DataView :value="products" layout="grid">
+    <template #header>
+      <h1>Products</h1>
+    </template>
+
+    <template #grid="slotProps">
+      <div class="grid grid-cols-12 gap-2">
+        <div v-for="(item, index) in slotProps.items" :key="index"
+          class="col-span-12 sm:col-span-6 md:col-span-4 xl:col-span-4 p-2">
+          <div
+            class="p-6 border border-surface-200 dark:border-surface-700 bg-surface-0 dark:bg-surface-900 rounded flex flex-col">
+            <div class="bg-surface-50 flex justify-center rounded p-4">
+              <div class="relative mx-auto">
+                <img class="rounded w-full" :src="img" :alt="item.productName" style="max-width: 300px" />
+                <div class="absolute" style="right: 4px; top: 4px">
+                  <Button icon="pi pi-plus" @click="cart.add(item)"></Button>
+                </div>
+              </div>
+            </div>
+            <div class="pt-6">
+              <div class="flex justify-between items-start gap-2">
+                <div>
+                  <span class="font-medium text-surface-500 dark:text-surface-400 text-sm">{{ item.brand }}</span>
+                  <div class="text-lg font-medium mt-1">{{ item.productName }}</div>
+                </div>
+              </div>
+              <div class="flex justity-between gap-6 mt-6">
+                <div class="text-2xl font-semibold">${{ item.listPrice }}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </DataView>
 </template>
 
 <script setup>
@@ -11,6 +41,7 @@ import { ref, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { ProductApi } from '../api/ProductApi';
 import { useCartStore } from '../store/CartStore';
+import img from './../assets/no_image_available.webp'
 
 const cart = useCartStore()
 
