7bab4c56e956217f7d8c1a72f55ddb14183964d1
Long Nguyen <longnewint@gmail.com>
2025-10-07 12:41:24 -0400

    Change cart api

--- src/api/CartApi.ts
+++ src/api/CartApi.ts
@@ -3,5 +3,9 @@ import ky from "ky"
 export const CartApi = {
   getData() {
     return ky.get("http://localhost:8080/cart/1234321").json()
+  },
+
+  post(id: number, qty: number) {
+    return ky.post("http://localhost:8080/cart/1234321", {json: {productId: id, quantity: qty}}).json
   }
 }
