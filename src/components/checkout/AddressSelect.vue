5e3ddd2379d4466532363f59e7e4da5f01ec3a0b
Long Nguyen <longnewint@gmail.com>
2025-10-11 21:05:17 -0400

    Add AddressSelect

---
+++ src/components/checkout/AddressSelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card flex">
+    <div class="flex flex-col gap-4">
+      <div>
+        <h1>Select shipping address</h1>
+      </div>
+      <div v-for="a in deliveryStore.addresses" class="flex gap-2">
+        <RadioButton v-model="deliveryStore.selected" :inputId="a.addressId" :value="a.addressId" />
+        <label :for="a.addressId">
+          {{ formatAddress(a.streetNumber, a.addressLine1, a.city, a.province, a.postalCode) }}
+        </label>
+      </div>
+      <div>
+        <Button variant="outlined" label="Add" @click="visible = true"></Button>
+      </div>
+    </div>
+  </div>
+  <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '30rem' }"
+    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+    <div>
+    <AddressForm @address-added="hideDialog"></AddressForm>
+    </div>
+  </Dialog>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useDeliveryStore } from '../../store/DeliveryStore';
+import AddressForm from './AddressForm.vue';
+
+const deliveryStore = useDeliveryStore()
+deliveryStore.fetchData()
+
+const visible = ref(false)
+
+function hideDialog() {
+  visible.value = false
+}
+
+function formatAddress(streetNumber, addressLine, city, province, postalCode) {
+  return `${streetNumber} ${addressLine}, ${city} ${province} ${postalCode}`
+}
+
+</script>
