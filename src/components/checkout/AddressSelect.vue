1c74eecd0dd37d96ceb7eb0ddaa5630144faf2ff
Long Nguyen <longnewint@gmail.com>
2025-10-21 11:05:25 -0400

    Asdfasdf

--- src/components/checkout/AddressSelect.vue
+++ src/components/checkout/AddressSelect.vue
@@ -5,7 +5,7 @@
         <h1>Select shipping address</h1>
       </div>
       <div v-for="a in deliveryStore.addresses" class="flex gap-2">
-        <RadioButton v-model="deliveryStore.selected" :inputId="a.addressId" :value="a.addressId" />
+        <RadioButton v-model="deliveryStore.selected" :inputId="a.addressId.toString()" :value="a.addressId" />
         <label :for="a.addressId">
           {{ formatAddress(a.streetNumber, a.addressLine1, a.city, a.province, a.postalCode) }}
         </label>
@@ -18,7 +18,7 @@
   <Dialog v-model:visible="visible" modal header="Header" :style="{ width: '30rem' }"
     :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
     <div>
-    <AddressForm @address-added="hideDialog"></AddressForm>
+      <AddressForm @address-added="hideDialog"></AddressForm>
     </div>
   </Dialog>
 </template>
@@ -27,14 +27,18 @@
 import { ref } from 'vue';
 import { useDeliveryStore } from '../../store/DeliveryStore';
 import AddressForm from './AddressForm.vue';
+import { useToast } from 'primevue';
 
 const deliveryStore = useDeliveryStore()
 deliveryStore.fetchData()
+
+const toast = useToast()
 
 const visible = ref(false)
 
 function hideDialog() {
   visible.value = false
+  toast.add({ severity: 'success', summary: `New address added`, life: 3000 })
 }
 
 function formatAddress(streetNumber, addressLine, city, province, postalCode) {
