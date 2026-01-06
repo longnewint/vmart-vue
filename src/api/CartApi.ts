9809a9fc09b3d5872c45d67528853e97db265042
Long Nguyen <longnewint@gmail.com>
2026-01-05 21:40:09 -0500

    ajsdhf

--- src/api/CartApi.ts
+++ src/api/CartApi.ts
@@ -1,11 +1,12 @@
 import ky from "ky"
+import { ApiConfig } from "./ApiConfig"
 
 export const CartApi = {
   getData() {
-    return ky.get("http://localhost:8080/cart/1234321").json()
+    return ky.get(ApiConfig.urlPath + "/cart/1234321").json()
   },
 
   post(id: number, qty: number) {
-    return ky.post("http://localhost:8080/cart/1234321", {json: {productId: id, quantity: qty}}).json
+    return ky.post(ApiConfig.urlPath + "/cart/1234321", {json: {productId: id, quantity: qty}}).json
   }
 }
