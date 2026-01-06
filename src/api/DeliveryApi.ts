0ea7f20a8c97e29a59510a38b0a79806aeba7312
Long Nguyen <longnewint@gmail.com>
2026-01-05 21:40:16 -0500

    klajsdfkjl

--- src/api/DeliveryApi.ts
+++ src/api/DeliveryApi.ts
@@ -1,13 +1,14 @@
 import ky from "ky"
+import { ApiConfig } from "./ApiConfig"
 import type { Address } from "../store/DeliveryStore"
 
 export const DeliveryApi = {
   get(customerId: number) {
-    return ky.get("http://localhost:8080/delivery/").json()
+    return ky.get(ApiConfig.urlPath + "/delivery/").json()
   },
 
   post(customerId: number, address: Address) {
-    return ky.post("http://localhost:8080/delivery/", {
+    return ky.post(ApiConfig.urlPath + "/delivery/", {
       json: {
         streetNumber: address.streetNumber,
         addressLine1: address.addressLine1,
